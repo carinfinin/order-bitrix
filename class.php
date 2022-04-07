3b83ec7b873e712f5fc5282f78a4ea3e4035d709
reschik <aleksejmelentev@gmail.com>
2022-04-07 23:45:03 +0300

    ajax

--- class.php
+++ class.php
@@ -2,9 +2,13 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
+require_once (__DIR__ . '/classes/Basket.php');
+require_once (__DIR__ . '/classes/BasketItem.php');
+require_once (__DIR__ . '/classes/Order.php');
 
 use \Bitrix\Main\Localization\Loc;
 use \Bitrix\Main\Loader;
+use \Bitrix\Sale\Registry;
 
 class customOrderComponent extends CBitrixComponent
 {
@@ -12,6 +16,8 @@ class customOrderComponent extends CBitrixComponent
      * @var \Bitrix\Sale\Order
      */
     public $order;
+
+    public $propMap = [];
 
     protected $errors = [];
 
@@ -40,6 +46,20 @@ class customOrderComponent extends CBitrixComponent
             }
         }
 
+        if (isset($arParams['IS_AJAX'])
+            && ($arParams['IS_AJAX'] == 'Y' || $arParams['IS_AJAX'] == 'N')) {
+            $arParams['IS_AJAX'] = $arParams['IS_AJAX'] == 'Y';
+        } else {
+            if (
+                isset($this->request['is_ajax'])
+                && ($this->request['is_ajax'] == 'Y' || $this->request['is_ajax'] == 'N')
+            ) {
+                $arParams['IS_AJAX'] = $this->request['is_ajax'] == 'Y';
+            } else {
+                $arParams['IS_AJAX'] = false;
+            }
+        }
+
         return $arParams;
     }
 
@@ -47,7 +67,18 @@ class customOrderComponent extends CBitrixComponent
     {
         global $USER;
 
+        // помена крзины
+        $registry = Registry::getInstance(Registry::REGISTRY_TYPE_ORDER);
+        $registry->set(Registry::ENTITY_BASKET, '\Local\Sale\Basket');
+        $registry->set(Registry::ENTITY_BASKET_ITEM, '\Local\Sale\BasketItem');
+
+        // помена класса заказа
+        $registry = Registry::getInstance(Registry::REGISTRY_TYPE_ORDER);
+        $registry->set(Registry::ENTITY_ORDER, '\Local\Sale\Order');
+
         try {
+
+
             $siteId = \Bitrix\Main\Context::getCurrent()->getSite();
             $basketItems = \Bitrix\Sale\Basket::loadItemsForFUser(
                 \CSaleBasket::GetBasketUserID(),
@@ -56,7 +87,7 @@ class customOrderComponent extends CBitrixComponent
                 ->getOrderableItems();
 
             if (count($basketItems) == 0) {
-                LocalRedirect(PATH_TO_BASKET);
+                LocalRedirect(PATH_TO_BASKET);  // указать ссылку на корзтну
             }
 
             $this->order = \Bitrix\Sale\Order::create($siteId, $USER->GetID());
@@ -110,11 +141,157 @@ class customOrderComponent extends CBitrixComponent
             $this->errors[] = $e->getMessage();
         }
     }
+
+    protected function calcAction()
+    {
+        $this->setTemplateName('');
+
+        //Собираем ID доставок
+        $deliveryIDs = [];
+        if (isset($this->request['delivery_id'])) {
+            if (is_array($this->request['delivery_id'])) {
+                foreach ($this->request['delivery_id'] as $val) {
+                    if (intval($val) > 0) {
+                        $deliveryIDs[intval($val)] = intval($val);
+                    }
+                }
+            } elseif (intval($this->request['delivery_id']) > 0) {
+                $deliveryIDs = [intval($this->request['delivery_id'])];
+            } else {
+                $deliveryIDs = [];
+            }
+
+        }
+        //На выходе в любом случае будет массив
+        sort($deliveryIDs);
+
+        if (empty($deliveryIDs)) {
+            throw new \Exception('Нет доставок для расчета');
+        }
+
+        $shipment = false;
+        /** @var \Bitrix\Sale\Shipment $shipmentItem */
+        foreach ($this->order->getShipmentCollection() as $shipmentItem) {
+            if (!$shipmentItem->isSystem()) {
+                $shipment = $shipmentItem;
+                break;
+            }
+        }
+
+        if (!$shipment) {
+            throw new \Exception('Отгрузка не найдена');
+        }
+
+        //Массив с доставками,
+        $availableDeliveries = \Bitrix\Sale\Delivery\Services\Manager::getRestrictedObjectsList(
+            $shipment
+        );
+
+        foreach ($deliveryIDs as $deliveryId) {
+            $obDelivery = false;
+            if (isset($availableDeliveries[$deliveryId])) {
+                //Если переданный из запроса ID доставки доступен покупателю
+                $obDelivery = $availableDeliveries[$deliveryId];
+            }
+
+            if ($obDelivery) {
+                $arDelivery = [
+                    'id'                => $obDelivery->getId(),
+                    'name'              => $obDelivery->getName(),
+                    'logo_path'         => $obDelivery->getLogotipPath(),
+                    'show'              => false,
+                    'calculated'        => false,
+                    'period'            => '',
+                    'price'             => 0,
+                    'price_formated'    => '',
+                ];
+
+                $shipment->setField('DELIVERY_ID', $obDelivery->getId());
+                $calcResult = $obDelivery->calculate($shipment);
+
+                if ($calcResult->isSuccess()) {
+                    $arDelivery['calculated'] = true;
+                    $arDelivery["price"] = $calcResult->getPrice();
+                    $arDelivery["price_formated"] = \SaleFormatCurrency(
+                        $calcResult->getPrice(),
+                        $this->order->getCurrency()
+                    );
+
+                    if (strlen($calcResult->getPeriodDescription()) > 0) {
+                        $arDelivery["period_text"] = $calcResult->getPeriodDescription();
+                    }
+                }
+
+                if (floatval($arDelivery['price']) > 0) {
+                    $arDelivery['show'] = true;
+                }
+
+                if (empty($arDelivery["period_text"])) {
+                    $arDelivery["period_text"] = '...';
+                }
+
+                $this->arResponse['deliveries'][$arDelivery['ID']] = $arDelivery;
+            } else {
+                //В аякс ответе, даже недоступную доставку возвращаем
+                $this->arResponse['deliveries'][$deliveryId] = [
+                    'id'   => $deliveryId,
+                    'show' => false
+                ];
+            }
+        }
+    }
+//
+//    public function getPriceDelivery()
+//    {
+//        $this->calcAction();
+//        $this->executeComponent();
+//
+//    }
+
+    public function getPropByCode($code)
+    {
+        $result = false;
+
+        $propId = 0;
+        if (isset($this->propMap[$code])) {
+            $propId = $this->propMap[$code];
+        }
+
+        if ($propId > 0) {
+            $result = $this->order
+                ->getPropertyCollection()
+                ->getItemByOrderPropertyId($propId);
+        }
+
+        return $result;
+    }
+
+    public function getPropDataByCode($code)
+    {
+        $result = [];
+
+        $propId = 0;
+        if (isset($this->propMap[$code])) {
+            $propId = $this->propMap[$code];
+        }
+
+        if ($propId > 0) {
+            $result = $this->order
+                ->getPropertyCollection()
+                ->getItemByOrderPropertyId($propId)
+                ->getFieldValues();
+        }
+
+        return $result;
+    }
+
     protected function setOrderProps()
     {
         global $USER;
         $arUser = $USER->GetByID(intval($USER->GetID()))
             ->Fetch();
+
+
 
         if (is_array($arUser)) {
             $fio = $arUser['LAST_NAME'] . ' ' . $arUser['NAME'] . ' ' . $arUser['SECOND_NAME'];
@@ -122,8 +299,10 @@ class customOrderComponent extends CBitrixComponent
             $arUser['FIO'] = $fio;
         }
 
+
         foreach ($this->order->getPropertyCollection() as $prop) {
             /** @var \Bitrix\Sale\PropertyValue $prop */
+            $this->propMap[$prop->getField('CODE')] = $prop->getPropertyId();
             $value = '';
 
             switch ($prop->getField('CODE')) {
@@ -161,13 +340,41 @@ class customOrderComponent extends CBitrixComponent
 
     function executeComponent()
     {
-        $this->createVirtualOrder();
-        if (isset($this->request['save']) && $this->request['save'] == 'Y') {
-            $this->order->save();
+        global $APPLICATION;
+
+        if ($this->arParams['IS_AJAX']) {
+            $APPLICATION->RestartBuffer();
         }
 
-        $this->includeComponentTemplate();
+        $this->createVirtualOrder();
 
+        if(!empty($this->arParams['ACTION'])) {
+            if (is_callable([$this, $this->arParams['ACTION'] . "Action"])) {
+                try {
+                    call_user_func([$this, $this->arParams['ACTION'] . "Action"]);
+                } catch (\Exception $e) {
+                    $this->errors[] = $e->getMessage();
+                }
+            }
+        }
+
+        if ($this->arParams['IS_AJAX']) {
+            if ($this->getTemplateName() != '') {
+                ob_start();
+                $this->includeComponentTemplate();
+                $this->arResponse['html'] = ob_get_contents();
+                ob_end_clean();
+            }
+
+            $this->arResponse['errors'] = $this->errors;
+
+            header('Content-Type: application/json');
+            echo json_encode($this->arResponse);
+            $APPLICATION->FinalActions();
+            die();
+        } else {
+            $this->includeComponentTemplate();
+        }
     }
 
 }
